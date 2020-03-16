fbaba30a2ae8223282b12eddebdfdc306a41e822
Brandon Dusch <brandondusch@gmail.com>
2020-03-16 11:59:00 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,4 +1,10 @@
 class Appointment
   attr_reader :patient, :doctor 
-  attr_accessor :
+  attr_accessor :date 
+  
+  def initialize(date, patient, doctor)
+    @date = date 
+    @patient = patient
+    @doctor = doctor
+  end
 end
